a50d1cd69d5ff6bd7f5a706bacdf75371fbf0b7c
Chase Patterson <chapatt@gmail.com>
2023-07-22 23:22:34 -0700

    feat: don't set view rect

--- xcode/modeler/AppDelegate.swift
+++ xcode/modeler/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.center()
         window.isMovableByWindowBackground = true
         window.backgroundColor = NSColor.clear
-        window.contentView = ModelerView(frame: rect)
+        window.contentView = ModelerView()
         window.makeKeyAndOrderFront(nil)
     }
 }
