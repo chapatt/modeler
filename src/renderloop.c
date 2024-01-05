c7de49aa42c21de968b042e40567bece2b45c8f4
Chase Patterson <chase.patterson@sourcetoad.com>
2024-01-05 02:11:30 -0800

    feat: add fps counter

--- src/renderloop.c
+++ src/renderloop.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "imgui/cimgui.h"
 #include "imgui/cimgui_impl_vulkan.h"
@@ -37,6 +38,11 @@ bool draw(VkDevice device, VkRenderPass renderPass, VkPipeline pipeline, VkFrame
 	}
 	cImGui_ImplVulkan_Init(&imVulkanInitInfo, renderPass);
 
+	struct timespec previousTime = {};
+	const size_t queueLength = 10;
+	long elapsedQueue[queueLength];
+	size_t head = 0;
+	long elapsed = 1;
 	for (uint32_t currentFrame = 0; true; currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT) {
 		VkResult result;
 
@@ -61,6 +67,18 @@ bool draw(VkDevice device, VkRenderPass renderPass, VkPipeline pipeline, VkFrame
 			}
 		}
 
+		struct timespec spec;
+		clock_gettime(CLOCK_MONOTONIC, &spec);
+			head = (head + 1) % queueLength;
+			elapsedQueue[head] = spec.tv_nsec + ((1000000000 * (spec.tv_sec - previousTime.tv_sec)) - previousTime.tv_nsec);
+		if (spec.tv_sec > previousTime.tv_sec) {
+			for (size_t i = 0; i < queueLength; ++i) {
+				elapsed = (elapsed + elapsedQueue[i]) / 2;
+			}
+		}
+		previousTime.tv_sec = spec.tv_sec;
+		previousTime.tv_nsec = spec.tv_nsec;
+
 		vkWaitForFences(device, 1, synchronizationInfo.frameInFlightFences + currentFrame, VK_TRUE, UINT64_MAX);
 		vkResetFences(device, 1, synchronizationInfo.frameInFlightFences + currentFrame);
 
@@ -77,7 +95,7 @@ bool draw(VkDevice device, VkRenderPass renderPass, VkPipeline pipeline, VkFrame
 		ImGui_ImplModeler_NewFrame();
 		ImGui_NewFrame();
 		ImGui_Begin("A Window", NULL, 0);
-		ImGui_Text("Hello, Window!");
+		ImGui_Text("fps: %ld", 1000000000 / elapsed);
 		ImGui_End();
 		ImGui_Render();
 		ImDrawData *drawData = ImGui_GetDrawData();
