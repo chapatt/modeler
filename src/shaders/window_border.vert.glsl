abcf16f951019c58c3bc9e4f77645eb5e1197ee1
Chase Patterson <chapatt@gmail.com>
2024-11-27 15:29:47 -0800

    style: add spaces around operators in glsl

--- src/shaders/window_border.vert.glsl
+++ src/shaders/window_border.vert.glsl
@@ -1,13 +1,13 @@
 #version 450
 
-layout(location=0) out vec3 fragColor;
+layout(location = 0) out vec3 fragColor;
 
 vec2 positions[3] = vec2[](
-	vec2(-1,3),
-	vec2(-1,-1),
-	vec2(3,-1)
+	vec2(-1, 3),
+	vec2(-1, -1),
+	vec2(3, -1)
 );
 
 void main() {
-	gl_Position=vec4(positions[gl_VertexIndex],0.0,1.0);
+	gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
 }
