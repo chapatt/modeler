a68fb86c7944c512bf23e0354d7cf2e21cf84c92
Chase Patterson <chapatt@gmail.com>
2024-12-16 09:12:11 -0800

    feat: cleanup buffers

--- src/buffer.c
+++ src/buffer.c
@@ -1,8 +1,10 @@
+#include <string.h>
+
 #include "buffer.h"
 #include "utils.h"
 #include "vulkan_utils.h"
 
-bool createBuffer(VkDevice device, VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags bufferUsage, VkMemoryPropertyFlags memoryUsage, VkBuffer *buffer, VmaAllocation *allocation, char **error)
+bool createBuffer(VkDevice device, VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags bufferUsage, VkMemoryPropertyFlags memoryUsage, VkMemoryPropertyFlags memoryFlags, VkBuffer *buffer, VmaAllocation *allocation, char **error)
 {
 	VkResult result;
 
@@ -14,7 +16,8 @@ bool createBuffer(VkDevice device, VmaAllocator allocator, VkDeviceSize size, Vk
 	};
 
 	VmaAllocationCreateInfo allocationCreateInfo = {
-		.usage = memoryUsage
+		.usage = memoryUsage,
+		.flags = memoryFlags,
 	};
 
 	if ((result = vmaCreateBuffer(allocator, &bufferCreateInfo, &allocationCreateInfo, buffer, allocation, NULL)) != VK_SUCCESS) {
@@ -27,25 +30,32 @@ bool createBuffer(VkDevice device, VmaAllocator allocator, VkDeviceSize size, Vk
 
 bool createVertexBuffer(VkDevice device, VmaAllocator allocator, VkCommandPool commandPool, VkQueue queue, VkBuffer *buffer, VmaAllocation *allocation, const Vertex *vertices, size_t vertexCount, char **error)
 {
+	VkResult result;
+
 	VkDeviceSize bufferSize = sizeof(*vertices) * vertexCount;
 
 	VkBuffer stagingBuffer;
 	VmaAllocation stagingBufferAllocation;
 
-	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &stagingBuffer, &stagingBufferAllocation, error)) {
+	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_AUTO, VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT, &stagingBuffer, &stagingBufferAllocation, error)) {
 		return false;
 	}
 
 	void *data;
-	vmaMapMemory(allocator, stagingBufferAllocation, &data);
+	if ((result = vmaMapMemory(allocator, stagingBufferAllocation, &data)) != VK_SUCCESS) {
+		asprintf(error, "Failed to map memory: %s", string_VkResult(result));
+		return false;
+	}
 	memcpy(data, vertices, (size_t) bufferSize);
 	vmaUnmapMemory(allocator, stagingBufferAllocation);
 
-	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, buffer, allocation, error)) {
+	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VMA_MEMORY_USAGE_AUTO, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, buffer, allocation, error)) {
 		return false;
 	}
 
-	copyBuffer(device, commandPool, queue, stagingBuffer, *buffer, bufferSize);
+	if (!copyBuffer(device, commandPool, queue, stagingBuffer, *buffer, bufferSize, error)) {
+		return false;
+	}
 
 	vmaDestroyBuffer(allocator, stagingBuffer, stagingBufferAllocation);
 
@@ -54,33 +64,42 @@ bool createVertexBuffer(VkDevice device, VmaAllocator allocator, VkCommandPool c
 
 bool createIndexBuffer(VkDevice device, VmaAllocator allocator, VkCommandPool commandPool, VkQueue queue, VkBuffer *buffer, VmaAllocation *allocation, const uint16_t *indices, size_t indexCount, char **error)
 {
+	VkResult result;
+
 	VkDeviceSize bufferSize = sizeof(*indices) * indexCount;
 
 	VkBuffer stagingBuffer;
 	VmaAllocation stagingBufferAllocation;
 
-	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &stagingBuffer, &stagingBufferAllocation, error)) {
+	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_AUTO, VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT, &stagingBuffer, &stagingBufferAllocation, error)) {
 		return false;
 	}
 
 	void *data;
-	vmaMapMemory(allocator, stagingBufferAllocation, &data);
+	if ((result = vmaMapMemory(allocator, stagingBufferAllocation, &data)) != VK_SUCCESS) {
+		asprintf(error, "Failed to map memory: %s", string_VkResult(result));
+		return false;
+	}
 	memcpy(data, indices, (size_t) bufferSize);
 	vmaUnmapMemory(allocator, stagingBufferAllocation);
 
-	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, buffer, allocation, error)) {
+	if (!createBuffer(device, allocator, bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VMA_MEMORY_USAGE_AUTO, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, buffer, allocation, error)) {
 		return false;
 	}
 
-	copyBuffer(device, commandPool, queue, stagingBuffer, *buffer, bufferSize);
+	if (!copyBuffer(device, commandPool, queue, stagingBuffer, *buffer, bufferSize, error)) {
+		return false;
+	}
 
 	vmaDestroyBuffer(allocator, stagingBuffer, stagingBufferAllocation);
 
 	return true;
 }
 
-void copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue queue, VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size)
+bool copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue queue, VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size, char **error)
 {
+	VkResult result;
+
 	VkCommandBufferAllocateInfo allocInfo = {
 		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
 		.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
@@ -89,14 +108,20 @@ void copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue queue, VkBuf
 	};
 
 	VkCommandBuffer commandBuffer;
-	vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+	if ((result = vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer)) != VK_SUCCESS) {
+		asprintf(error, "Failed to allocate command buffers: %s", string_VkResult(result));
+		return false;
+	}
 
 	VkCommandBufferBeginInfo beginInfo = {
 		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
 		.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
 	};
 
-	vkBeginCommandBuffer(commandBuffer, &beginInfo);
+	if ((result = vkBeginCommandBuffer(commandBuffer, &beginInfo)) != VK_SUCCESS) {
+		asprintf(error, "Failed to begin command buffer: %s", string_VkResult(result));
+		return false;
+	}
 
 	VkBufferCopy copyRegion = {
 		.srcOffset = 0,
@@ -106,7 +131,10 @@ void copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue queue, VkBuf
 
 	vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
 
-	vkEndCommandBuffer(commandBuffer);
+	if ((result = vkEndCommandBuffer(commandBuffer)) != VK_SUCCESS) {
+		asprintf(error, "Failed to end command buffer: %s", string_VkResult(result));
+		return false;
+	}
 
 	VkSubmitInfo submitInfo = {
 		.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
@@ -114,10 +142,18 @@ void copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue queue, VkBuf
 		.pCommandBuffers = &commandBuffer,
 	};
 
-	vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE);
-	vkQueueWaitIdle(queue);
+	if ((result = vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE)) != VK_SUCCESS) {
+		asprintf(error, "Failed to submit queue: %s", string_VkResult(result));
+		return false;
+	}
+	if ((result = vkQueueWaitIdle(queue)) != VK_SUCCESS) {
+		asprintf(error, "Failed to wait for queue to be idle: %s", string_VkResult(result));
+		return false;
+	}
 
 	vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+
+	return true;
 }
 
 void destroyBuffer(VmaAllocator allocator, VkBuffer buffer, VmaAllocation allocation)
