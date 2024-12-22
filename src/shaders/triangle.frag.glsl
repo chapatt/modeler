1edc31350706f6ff6ceaef6d2f8546daf59e70b2
Chase Patterson <chapatt@gmail.com>
2024-12-21 18:12:16 -0800

    feat: draw pieces from sprite sheet

--- src/shaders/triangle.frag.glsl
+++ src/shaders/triangle.frag.glsl
@@ -7,6 +7,7 @@ layout(binding = 0) uniform sampler2D texSampler;
 layout(location = 0) out vec4 outColor;
 
 void main() {
-	outColor = vec4(fragColor, 1.0);
-	outColor = texture(texSampler, fragTexCoord);
+	vec4 background = vec4(fragColor, 1.0);
+	vec4 texture = texture(texSampler, fragTexCoord);
+	outColor = mix(background, vec4(texture.rgb, 1.0), texture.a);
 }
