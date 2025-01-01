9b98537714a3c2c4cf9307865775faccff64a3fa
Chase Patterson <chapatt@gmail.com>
2025-01-01 09:41:23 -0800

    chore: remove unused file

--- src/surface_wayland.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODELER_SURFACE_WAYLAND_H
-#define MODELER_SURFACE_WAYLAND_H
-
-#include <stdbool.h>
-#include <vulkan/vulkan.h>
-
-bool createSurfaceWayland(VkInstance instance, struct wl_display *waylandDisplay, struct wl_surface *waylandSurface, VkSurfaceKHR *surface, char **error);
-
-#endif /* MODELER_SURFACE_WAYLAND_H */
