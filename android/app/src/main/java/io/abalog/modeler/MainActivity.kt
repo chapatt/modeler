bec4894a0b2d7cf0b61b57bfe9c71eaa4d8a787c
Chase Patterson <chapatt@gmail.com>
2025-01-02 15:28:43 -0800

    feat: change theme on android to show status bar

--- android/app/src/main/java/io/abalog/modeler/MainActivity.kt
+++ android/app/src/main/java/io/abalog/modeler/MainActivity.kt
@@ -9,21 +9,4 @@ class MainActivity : GameActivity() {
             System.loadLibrary("modeler")
         }
     }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUi()
-        }
-    }
-
-    private fun hideSystemUi() {
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
 }
