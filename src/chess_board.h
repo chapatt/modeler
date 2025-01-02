dabf5b562fdba73dc63338689e976e9dc3ea13e8
Chase Patterson <chapatt@gmail.com>
2025-01-02 14:51:26 -0800

    chore: remove unused member variable and args

--- src/chess_board.h
+++ src/chess_board.h
@@ -13,10 +13,10 @@ typedef struct chess_board_t *ChessBoard;
 #include "vulkan_utils.h"
 #include "vk_mem_alloc.h"
 
-bool createChessBoard(ChessBoard *chessBoard, ChessEngine engine, VkDevice device, VmaAllocator allocator, VkCommandPool commandPool, VkQueue queue, VkRenderPass renderPass, uint32_t subpass, const char *resourcePath, float aspectRatio, float width, float originX, float originY, char **error);
+bool createChessBoard(ChessBoard *chessBoard, ChessEngine engine, VkDevice device, VmaAllocator allocator, VkCommandPool commandPool, VkQueue queue, VkRenderPass renderPass, uint32_t subpass, const char *resourcePath, float width, float originX, float originY, char **error);
 bool drawChessBoard(ChessBoard self, VkCommandBuffer commandBuffer, char **error);
 void destroyChessBoard(ChessBoard self);
-void setSize(ChessBoard self, float aspectRatio, float width, float originX, float originY);
+void setSize(ChessBoard self, float width, float originX, float originY);
 void setBoard(ChessBoard self, Board8x8 board);
 void setMove(ChessBoard self, MoveBoard8x8 move);
 bool updateChessBoard(ChessBoard self, char **error);
