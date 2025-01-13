0a435f2a33bcea7d3d00ccf7b92beaecb0772356
Chase Patterson <chapatt@gmail.com>
2025-01-13 13:07:26 -0800

    feat: hide status bar when landscape on android

--- android/app/src/main/java/io/abalog/modeler/MainActivity.kt
+++ android/app/src/main/java/io/abalog/modeler/MainActivity.kt
@@ -1,12 +1,30 @@
 package io.abalog.modeler
 
-import android.view.View
 import com.google.androidgamesdk.GameActivity
+import android.view.WindowInsets
+import android.content.res.Configuration
 
 class MainActivity : GameActivity() {
     companion object {
         init {
             System.loadLibrary("modeler")
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            hideSystemUi()
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            showSystemUi()
+        }
+    }
+
+    private fun hideSystemUi() {
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
+    }
+
+    private fun showSystemUi() {
+        window.insetsController?.show(WindowInsets.Type.statusBars())
     }
 }
