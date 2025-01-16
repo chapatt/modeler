84ac186b43999478cd93d10c8473f65958d21bef
Chase Patterson <chapatt@gmail.com>
2025-01-15 17:53:14 -0800

    feat: migrate to swiftui on ios

---
+++ xcode/modeler-ios/ModelerViewRepresentable.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ModelerViewRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> ModelerView {
+        ModelerView()
+    }
+    
+    func updateUIView(_ uiView: ModelerView, context: Context) {
+    }
+}
