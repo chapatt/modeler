97ac4c59bc7fbeabfb0573cbf70935030a7b24d3
Chase Patterson <chapatt@gmail.com>
2025-01-15 16:56:47 -0800

    feat: render ios at native scale

--- xcode/modeler-ios/ModelerViewController.swift
+++ xcode/modeler-ios/ModelerViewController.swift
@@ -2,16 +2,6 @@ import UIKit
 
 class ModelerViewController: UIViewController {
     override func loadView() {
-        self.view = ModelerView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if let view = self.view as? ModelerView {
-            view.startRenderloop()
-        }
+        self.view = ModelerView()
     }
 }
