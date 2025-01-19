af83b465be16a3df22288d9cf4c5297d9c05daf7
Chase Patterson <chapatt@gmail.com>
2025-01-19 13:35:02 -0800

    feat: destroy and init vulkan on ios background and foreground

--- xcode/modeler-ios/ModelerViewController.swift
+++ xcode/modeler-ios/ModelerViewController.swift
@@ -11,9 +11,21 @@ class ModelerViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         (view as! ModelerView).startVulkanThread()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleWillEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleDidEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
+        (view as! ModelerView).terminateVulkanThread()
+    }
+    
+    @objc func handleWillEnterForeground() {
+        (view as! ModelerView).startVulkanThread()
+    }
+    
+    @objc func handleDidEnterBackground() {
         (view as! ModelerView).terminateVulkanThread()
     }
 }
