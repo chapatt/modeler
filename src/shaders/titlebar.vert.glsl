c531861ea68e32bb9aae4ea15a2b8c4dd9a6e657
Chase Patterson <chapatt@gmail.com>
2025-03-28 14:24:29 -0700

    feat: turn off depth testing for titlebar

--- src/shaders/titlebar.vert.glsl
+++ src/shaders/titlebar.vert.glsl
@@ -14,10 +14,10 @@ float height = PushConstants.height;
 float width = height * 2;
 
 vec2 positions[] = vec2[](
+	vec2(-1, -1), vec2(-1, height - 1.0), vec2(1, height - 1.0), vec2(1, -1),
 	vec2(1.0 - width, -1), vec2(1.0 - width, height - 1.0), vec2(1, height - 1.0), vec2(1, -1),
 	vec2(1.0 - width * 2, -1), vec2(1.0 - width * 2, height - 1.0), vec2(1 - width, height - 1.0), vec2(1 - width, -1),
-	vec2(1.0 - width * 3, -1), vec2(1.0 - width * 3, height - 1.0), vec2(1 - width * 2, height - 1.0), vec2(1 - width * 2, -1),
-	vec2(-1, -1), vec2(-1, height - 1.0), vec2(1, height - 1.0), vec2(1, -1)
+	vec2(1.0 - width * 3, -1), vec2(1.0 - width * 3, height - 1.0), vec2(1 - width * 2, height - 1.0), vec2(1 - width * 2, -1)
 );
 
 int indices[] = int[](
@@ -30,13 +30,13 @@ int indices[] = int[](
 void main() {
 	gl_Position = vec4(positions[indices[gl_VertexIndex]], 0.0, 1.0);
 	if (gl_VertexIndex < 6) {
-		fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		fragColor = vec4(1.0, 1.0, 1.0, 0.2);
 	} else if (gl_VertexIndex < 12) {
 		fragColor = vec4(0.0, 1.0, 0.0, 1.0);
 	} else if (gl_VertexIndex < 18) {
 		fragColor = vec4(0.0, 0.0, 1.0, 1.0);
 	} else if (gl_VertexIndex < 24) {
-		fragColor = vec4(1.0, 1.0, 1.0, 0.2);
+		fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 	}
 	fragTexCoord = vec2(0.0, 0.0);
 }
