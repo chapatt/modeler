9543393c6a54755b3b524140c079f055b48777e9
Chase Patterson <chapatt@gmail.com>
2026-06-08 19:29:34 -0700

    build: update android build version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.abalog.chess"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
